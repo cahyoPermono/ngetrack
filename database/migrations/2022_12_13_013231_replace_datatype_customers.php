236038a06c75bb2d10b98f2398d9fc2e3ddfb15e
Cahyo <cahyo.adi.permono@gmail.com>
2022-12-14 11:39:29 +0700

    fixing customer migrations

--- database/migrations/2022_12_13_013231_replace_datatype_customers.php
+++ database/migrations/2022_12_13_013231_replace_datatype_customers.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->string('phone');
+            $table->string('phone')->change();
         });
     }
 
