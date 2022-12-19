e1bb7ca2363aece7fc41b31a99b1810e94bec61a
Cahyo <cahyo.adi.permono@gmail.com>
2022-12-19 08:43:14 +0700

    fixing landing page

--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,12 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('', [DashboardController::class, 'index'])->name('dashboard');
 
     // Routing Vehicles
     Route::get('vehicles', [VehicleController::class, 'index'])->name('vehicles');
