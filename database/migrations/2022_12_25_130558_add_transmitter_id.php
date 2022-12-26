95b4702425e96ac18a2f27fa3ccbce2643549ead
Cahyo <cahyo.adi.permono@gmail.com>
2022-12-26 07:32:09 +0700

    adding relationship routes to transmitter

---
+++ database/migrations/2022_12_25_130558_add_transmitter_id.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vehicle_routes', function (Blueprint $table) {
+            $table->bigInteger('transmitter_id')->unsigned();
+            // $table->foreign('transmitter_id')
+            //         ->references('id')
+            //         ->on('transmitters')
+            //         ->onCascade('delete'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vehicle_routes', function (Blueprint $table) {
+            // $table->dropForeign(['transmitter_id']);
+            $table->dropColumn('transmitter_id');
+        });
+    }
+};
