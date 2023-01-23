860407c8ae21a46a9be9c40352627fd891c73f63
Cahyo <cahyo.adi.permono@gmail.com>
2023-01-23 11:20:48 +0700

    Adding migration

---
+++ database/migrations/2023_01_23_041843_create_non_route_trackings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('non_route_trackings', function (Blueprint $table) {
+            $table->id();
+            $table->string('imei');
+            $table->string('team_id');
+            $table->string('long');
+            $table->string('lat');
+            $table->string('speed');
+            $table->string('heading');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('non_route_trackings');
+    }
+};
